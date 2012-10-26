c3c3d876ffd6055f10c936af4c345775b5630dba
Anatoly Smol <zarkone@ya.ru>
2012-10-26 16:18:18 +0700

    расписал функции еще дальше, пока не показывал

--- l2/l2.cpp
+++ l2/l2.cpp
@@ -2,38 +2,62 @@
 #include <sstream>
 using namespace std;
 
-int sg(int x){ return x>0; }
+long int fplus(long int x, long int y){
+  if(!y) return x;
+  return fplus(x, y-1)+1;
+}
+long int mul(long int x, long int y){
+  if(!y) return 0;
+  return fplus(mul(x,y-1), x);
+}
+
+long int uminus(long int x, long int y){
+  if(!x) return 0;
+  if(!y) return x;
+  
+  return uminus(x-1, y-1);
+}
+
+int sg(int x){ return uminus(x,0); }
+
 long int fact(int n){
   if (!n) return 1;
-  else return n*fact(n-1);
+  else return mul(n,fact(n-1));
 }
 
 long int min (long int x, long int y){
-  if (x > y) return y;
+  if (uminus(x,y)) return y;
   else return x;
 }
 
+long int ost (long int x, long int d){
+  if(x == d) return 0;
+  if(uminus(x,d) == 0) return x;
+  if(uminus(d,x) == 0) return ost(uminus(x,d), d);
+}
+
+//
 long int Pi (long int x, long int y, long int z){
-  return !(x % z || y % z);
+  return !(ost(x,z) || ost(y,z));
 }
 
 long int d (long int x, long int y){
   
   long int z=min(x,y);
-  while (!Pi(x, y, z) && z>0) z--;
+  while (!Pi(x, y, z) && uminus(z,0)) z--;
   return z;
 }
 
 long int e (long int x, long int d){
   long int z=0;
-  while(z*d < x) z++;
+  while( uminus(x, mul(z,d)) ) z++;
   return z;
 }
 
 long int f (long int ch, long int zn, int n){
   if(!n)
     return ch;
-  return  f(ch*n+zn, zn*n, n-1) ;  
+  return  f(fplus(mul(ch,n),zn), mul(zn,n), n-1) ;  
 }
 
 long int F (int n){
