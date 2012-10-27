bc80ede8d236d395bf2c30af305728e343e7623f
Anatoly Smol <zarkone@ya.ru>
2012-10-27 11:22:34 +0700

    Сдал вторую

--- l2/l2.cpp
+++ l2/l2.cpp
@@ -2,6 +2,7 @@
 #include <sstream>
 using namespace std;
 
+
 long int fplus(long int x, long int y){
   if(!y) return x;
   return fplus(x, y-1)+1;
@@ -11,63 +12,50 @@ long int mul(long int x, long int y){
   return fplus(mul(x,y-1), x);
 }
 
-long int uminus(long int x, long int y){
-  if(!x) return 0;
-  if(!y) return x;
-  
-  return uminus(x-1, y-1);
-}
 
-int sg(int x){ return uminus(x,0); }
+int sg(int x){ return x>0; }
 
 long int fact(int n){
   if (!n) return 1;
   else return mul(n,fact(n-1));
 }
 
-long int min (long int x, long int y){
-  if (uminus(x,y)) return y;
-  else return x;
-}
-
-long int ost (long int x, long int d){
-  if(x == d) return 0;
-  if(uminus(x,d) == 0) return x;
-  if(uminus(d,x) == 0) return ost(uminus(x,d), d);
-}
-
-//
 long int Pi (long int x, long int y, long int z){
-  return !(ost(x,z) || ost(y,z));
+  return !sg(fplus(x%(y-z), y%(y-z)));
 }
 
 long int d (long int x, long int y){
   
-  long int z=min(x,y);
-  while (!Pi(x, y, z) && uminus(z,0)) z--;
+  long int z=0;
+  while (!Pi(x, y, z) && z<y) z++;
   return z;
 }
-
+// [x/y]
 long int e (long int x, long int d){
   long int z=0;
-  while( uminus(x, mul(z,d)) ) z++;
+  while(z < x+1 && !sg( (z+1)*d-x ) ) z++;
   return z;
 }
+// x % y
+long int g (long int x, long int d){
+  
+  return (x-e(x,d)*d)*sg(x-d+1);
+}
 
-long int f (long int ch, long int zn, int n){
-  if(!n)
-    return ch;
-  return  f(fplus(mul(ch,n),zn), mul(zn,n), n-1) ;  
+long int f( int n){
+  if(!n) return 0;
+  return mul(f(n-1),n)+fact(n-1);
 }
 
 long int F (int n){
+  
   return 
         e(
-            f(1, n, n-1), 
-            d( 
-              f(1, n, n-1), 
-              fact(n) 
-            ) 
+            f(n), 
+            fact(n)-d( 
+                      f(n), 
+                      fact(n) 
+                    ) 
         );
 }
 
@@ -79,7 +67,6 @@ int main(int argc, char *argv[])
     arg << argv[1];
     arg >> n; 
     
-    cout << F(n)<< endl;
-
+    cout <<F(n)<< endl;
 }
 
